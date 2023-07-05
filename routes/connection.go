5c6334ae1bc9e4345316a58829d92d28517f02ba
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-05 11:51:37 +0300

    mongoDB connection

--- routes/connection.go
+++ routes/connection.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func DBInstance() error {
+	MongoDb := "mongodb://localhost:27017/caloriesDb"
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		return err
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return err
+	}
+
+	Client = client
+	fmt.Println("Connected to MongoDB")
+	return nil
+}
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("caloriesDb").Collection(collectionName)
+	return collection
+}
