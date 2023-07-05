a083fb9da1ae6cf5b80590f36e2e082b7cf960d9
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-05 10:52:36 +0300

    getEntries

--- routes/entries.go
+++ routes/entries.go
@@ -18,7 +18,23 @@ func AddEntry(c *gin.Context) {
 }
 
 func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M{})
 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		//return
+	}
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		//return
+	}
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
 }
 
 func GetEntryById(c *gin.Context) {
