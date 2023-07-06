1ad3d850fceaf3222ff246bb0167349e0d676b8f
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-06 10:58:46 +0300

    fix connection to mongodb

--- routes/connection.go
+++ routes/connection.go
@@ -5,23 +5,30 @@ import (
 	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
 )
 
-func DBInstance() (*mongo.Client, error) {
+func DBInstance() *mongo.Client {
 	MongoDb := "mongodb://localhost:27017/caloriesDb"
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoDb))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDb))
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(ctx, nil)
+
+	//err = client.Connect(ctx)
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
 	fmt.Println("Connected to MongoDB")
-	return client, nil
+	return client
 }
 
 var Client *mongo.Client = DBInstance()
